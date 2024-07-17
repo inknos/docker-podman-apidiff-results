0bae74ec56b90e4aaea1ff5ff0e49b79ddbd63d6
Nicola Sella <nsella@redhat.com>
2024-07-17 12:14:07 +0200

    add table

--- get-specs.sh
+++ get-specs.sh
@@ -6,8 +6,11 @@ docker_versions=(
 	1.43
 	1.42
 	1.41
-	1.40	
+	1.40
 )
+
+mkdir specs
+mkdir results
 
 wget -O "specs/podman-latest.yaml" "https://storage.googleapis.com/libpod-master-releases/swagger-latest.yaml"
 
@@ -15,8 +18,6 @@ for version in "${docker_versions[@]}"; do
 	url="https://docs.docker.com/reference/engine/v${version}.yaml"
 	wget -O "specs/docker-v${version}.yaml" "$url"
 done
-
-
 
 # Compare Podman specs with Docker specs
 for version in "${docker_versions[@]}"; do
@@ -30,3 +31,22 @@ for version in "${docker_versions[@]}"; do
 		--html "/results/html/$output.html"
 done
 
+# Compare Docker specs with Docker specs
+for version in ${docker_versions[@]}; do
+    for other_version in ${docker_versions[@]}; do
+        # continue loop if same version and just touch the file
+        if [ "$version" = "$other_version" ]; then
+            touch "results/markdown/$output.md"
+            touch "results/html/$output.html"
+            continue
+        fi
+        output="docker-v$version-docker-v$other_version"
+        podman run --rm -t \
+            -v "$(pwd)/specs:/specs:ro,Z" \
+            -v "$(pwd)/results:/results:Z" \
+            openapitools/openapi-diff:latest \
+            "/specs/docker-v$version.yaml" "/specs/docker-v$other_version.yaml" \
+            --markdown "/results/markdown/$output.md" \
+            --html "/results/html/$output.html"
+        done
+    done
